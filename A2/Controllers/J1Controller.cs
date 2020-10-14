869056c342e2da01b87ab61f659340f2cb366dcd
naihsienchen <nhc0629@gmail.com>
2020-10-14 14:03:57 -0400

    Add project files.

---
+++ A2/Controllers/J1Controller.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace A2.Controllers
+{
+    public class J1Controller : ApiController
+    {
+
+        [Route("api/J1/Menu/{burger}/{drink}/{side}/{dessert}")]
+        [HttpGet]
+        public IEnumerable<string> Menu(int burger, int drink, int side, int dessert)
+        {
+            int burgercal = 0;
+            int drinkcal = 0;
+            int sidecal = 0;
+            int descal = 0;
+
+            switch (burger)
+            {
+                case 1:
+                    burgercal = 461;
+                    break;
+                case 2:
+                    burgercal = 431;
+                    break;
+                case 3:
+                    burgercal = 420;
+                    break;
+                default:
+                case 4:
+                    burgercal = 0;
+                    break;
+            };
+
+            switch (drink)
+            {
+                case 1:
+                    drinkcal = 130;
+                    break;
+                case 2:
+                    drinkcal = 160;
+                    break;
+                case 3:
+                    drinkcal = 118;
+                    break;
+                default:
+                case 4:
+                    drinkcal = 0;
+                    break;
+            };
+
+            switch (side)
+            {
+                case 1:
+                    sidecal = 100;
+                    break;
+                case 2:
+                    sidecal = 57;
+                    break;
+                case 3:
+                    sidecal = 70;
+                    break;
+                default:
+                case 4:
+                    sidecal = 0;
+                    break;
+            };
+
+            switch (dessert)
+            {
+                case 1:
+                    descal = 167;
+                    break;
+                case 2:
+                    descal = 266;
+                    break;
+                case 3:
+                    descal = 75;
+                    break;
+                default:
+                case 4:
+                    descal = 0;
+                    break;
+            };
+            int totalcal = burgercal + drinkcal + sidecal + descal;
+            return new string[] { "Your total calorie count is " + totalcal };
+        }
+    }
+}
---
+++ A2/Controllers/J2Controller.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace A2.Controllers
+{
+    public class J2Controller : ApiController
+    {
+        /// <summary>
+        /// Processing the number of ways two dice can roll the value of 10.
+        /// </summary>
+        /// <param name="m">total sides of 1st die</param>
+        /// <param name="n">total sides of 2nd die</param>
+        /// <returns>A string explaining how many ways</returns>
+        [Route("api/J2/DiceGame/{m}/{n}")]
+        [HttpGet]
+        public IEnumerable<string> DiceGame(int m, int n)
+        {
+            int count = 0;
+            int i = 0;
+            int j = 0;
+            for (i = 0; i <= m; i++)
+            {
+                for (j = 0; j <= n; j++)
+                {
+                    if (i + j == 10) count = count + 1;
+                };
+            };
+            return new string[] { "There are a total of " + count + " ways to get the sum of 10." };
+        }
+    }
+}
